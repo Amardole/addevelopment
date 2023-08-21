3822a5994d0d9870e60e5227fd5ab707a755477a
Amardole <96816266+Amardole@users.noreply.github.com>
2023-08-21 13:07:23 +0530

    Add files via upload

---
+++ scriptcombo.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const applyCouponButton = document.getElementById('apply-coupon');
+    const couponCodeInput = document.getElementById('coupon-code');
+    const finalPriceElement = document.getElementById('final-price');
+    const originalPrice = 1800; // Original course price
+  
+    applyCouponButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+  
+      const couponCode = couponCodeInput.value;
+      if (couponCode === '500') {
+        const discountedPrice = originalPrice - 900;
+        finalPriceElement.textContent = `Final Price: $${discountedPrice}`;
+      } else {
+        finalPriceElement.textContent = `Final Price: $${originalPrice}`;
+      }
+  
+      finalPriceElement.classList.remove('hidden'); // Show the final price
+    });
+  });
+  document.getElementById("btn").addEventListener("click", function() {
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSeV21zn93fq88LssiO09H8HzVgrUZVfkwdMSIJElNqc0HEduw/viewform?usp=sf_link", "_blank");
+  });
+
+  
